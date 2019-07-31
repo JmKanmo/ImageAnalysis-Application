3e8948f6c7370ab1bd2bf5b180273082f75d3fd9
JmKanmo <nebi25@naver.com>
2019-07-31 21:22:37 +0900

    Camera , gallery permission setting

--- app/src/main/java/com/junmo/imagevisionapplication/MainActivity.kt
+++ app/src/main/java/com/junmo/imagevisionapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.junmo.imagevisionapplication
 
+import android.Manifest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,26 +8,45 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val CAMERA_PERMISSION_REQUEST = 1000
+    private val GALLERY_PERMISSION_REQUEST = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupListener()
     }
 
-    private fun setupListener(){
+    private fun setupListener() {
         upload_image.setOnClickListener {
             UploadChooser().apply {
-                addNotifier(object:UploadChooser.UploadChooserNotifierInterface{
+                addNotifier(object : UploadChooser.UploadChooserNotifierInterface {
                     override fun cameraOnClick() {
-                        Log.d("TAG","카메라클릭작동")
+                        Log.d("TAG", "카메라클릭작동")
+                        //카메라권한
+                        checkCameraPermission()
                     }
+
                     override fun galleryOnClick() {
-                        Log.d("TAG","갤러리클릭작동")
+                        Log.d("TAG", "갤러리클릭작동")
+                        //저장장치권한
+                        checkGalleryPermission()
                     }
                 })
-            }.show(supportFragmentManager,"")
+            }.show(supportFragmentManager, "")
             //UploadChooser().show(supportFragmentManager,"")
         }
     }
 
+    private fun checkCameraPermission() {
+        PermissionUtility().requestPermission(
+            this, CAMERA_PERMISSION_REQUEST, Manifest.permission.CAMERA
+        )
+    }
+
+    private fun checkGalleryPermission() {
+        PermissionUtility().requestPermission(
+            this, GALLERY_PERMISSION_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA
+        )
+    }
 }
---
+++ app/src/main/java/com/junmo/imagevisionapplication/PermissionUtility.kt
@@ -0,0 +1,32 @@
+package com.junmo.imagevisionapplication
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import java.security.Permissions
+
+class PermissionUtility {
+    fun requestPermission(
+        activity: Activity, requestCode: Int, vararg permissions: String
+    ): Boolean {
+        var granted = true
+        val permissionNeeded = ArrayList<String>()
+        permissions.forEach {
+            val permissionCheck = ContextCompat.checkSelfPermission(activity, it)
+            val hasPermission = permissionCheck == PackageManager.PERMISSION_GRANTED
+            granted = granted and hasPermission
+            if (!hasPermission) {
+                permissionNeeded.add(it)
+            }
+        }
+        if (granted) {
+            return true
+        } else {
+            ActivityCompat.requestPermissions(
+                activity, permissionNeeded.toTypedArray(), requestCode
+            )
+            return false
+        }
+    }
+}
